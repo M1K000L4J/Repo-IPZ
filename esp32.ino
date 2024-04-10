a0f2940577eac374a570194138d481760a0be857
WojtekKaminski071 <162632187+WojtekKaminski071@users.noreply.github.com>
2024-04-10 18:06:05 +0200

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -1,25 +1,35 @@
 #include <Wire.h>
 
 #define AS5600 0x36 // Adres enkodera AS5600 do i2c
+
 //Pinouty pinów zasilających poszczególne enkodery
+#define enkoder_1_power GPIO_NR
+#define enkoder_2_power GPIO_NR
+#define enkoder_3_power GPIO_NR
+#define enkoder_4_power GPIO_NR
 
 void setup() {
   Wire.begin(); // Inicjalizacja interfejsu I2C
   Serial.begin(9600); // Inicjalizacja komunikacji szeregowej
 }
 
-double read(int enkoder_power_pin){
+double read(double enkoder_power_pin){
+  enkoder_power_pin = 1;
   Wire.beginTransmission(AS5600); // Rozpoczęcie transmisji do enkodera
   Wire.write(0x0E); // Adres rejestru odczytu kąta
   Wire.endTransmission(); // Zakończenie transmisji
 
   Wire.requestFrom(AS5600_ADDR, 2); // Odczytaj 2 bajty danych z enkodera
-  if (Wire.available() >= 2) {
+  if (Wire.available() == 2) {
     int angle_LSB = Wire.read(); // Odczytaj mniej znaczący bajt
     int angle_MSB = Wire.read(); // Odczytaj bardziej znaczący bajt
 
     int angle = (angle_MSB << 8) | angle_LSB; // Oblicz wartość kąta
     return(angle);
+  }
+  else 
+    return(-1);
+  enkoder_power_pin = 0;
 }
 
 
